f1f587f35c0bd209d1275be533830a82f7917efa
Noah <niniguezg@gmail.com>
2024-04-18 09:51:05 -0500

    Simplify by using official Octoprint image

--- octoprint/nginx-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
-    -subj "/CN=octoprint" \
-    -keyout /etc/ssl/private/nginx-selfsigned.key  \
-    -out /etc/ssl/certs/nginx-selfsigned.crt \
-    > /dev/null
